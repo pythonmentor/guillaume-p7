8f7b45a2f878d6296dd92b6dc14e60aed74c0877
Jc4Mn <guillaumecarcema@gmail.com>
2019-06-12 18:29:43 +0200

    Adding functions lowers and rm_accents in class ParseSentence

--- robot/parse.py
+++ robot/parse.py
@@ -3,6 +3,12 @@
 # tag words.
 ####################################################################
 
+# import modules
+import unicodedata
+import re
+
+#personnal modules
+
 class ParseSentence (object) :
     """This class parses a sentence and gives back
     tag words"""
@@ -11,11 +17,30 @@ class ParseSentence (object) :
     def __init__(self, sentence):
         self.sentence=sentence
 
-    # function testing pytest compatibility
-    def backword(self) :
+    # function lowering sentence letters
+    def lowers(self) :
+        """This function will return every upper character
+        in this sentence to lower ones"""
+        # lowers every upper character in the sentence
+        self.sentence=self.sentence.lower()
+
+        # used for the test only
         return self.sentence
 
-#if __name__ == '__main__' :
-#    K=ParseSentence("jean")
-#    K.backword()
+    def rm_accents(self) :
+        """This function removes accents and replaces various characters by space"""
+        # removes every accents and non letters characters
+        self.sentence = unicodedata.normalize('NFD', self.sentence).encode('ascii', 'ignore')
+        self.sentence = self.sentence.decode("utf8")
+
+        #replaces special characters by spaces
+        self.sentence = re.sub(r'\W'," ",self.sentence)
+        #used for the test only
+
+        #used for the test only
+        return self.sentence
+
+
+if __name__ == '__main__' :
+    pass
 
--- test/test_parse.py
+++ test/test_parse.py
@@ -3,15 +3,30 @@ from robot.parse import ParseSentence
 import pytest
 
 class TestParseSentence:
-    #sentence test
+    #sentence test    
     @pytest.mark.parametrize('a,result', [
-        ("jean", "jean"),
-        ("claude", "claude"),
-        ("blabli","blabli")
+        ("Bonjour GrandPy, Que sais tu sur la tour eiffel ?", 
+        "bonjour grandpy, que sais tu sur la tour eiffel ?"),
+        ("Salut GrandPy ! Est-ce que tu connais l'adresse d'OpenClassrooms ?",
+         "salut grandpy ! est-ce que tu connais l'adresse d'openclassrooms ?"),
+        ("Commènt vas tu GràndPy, ça va bien o`↓ pas ?",
+        "commènt vas tu gràndpy, ça va bien o`↓ pas ?")
     ])
 
-    def test_backword(self, a, result):
-
+    def test_lowers(self, a, result):
         parser = ParseSentence(a)
-        assert parser.backword() == result
+        assert parser.lowers() == result
+    
+    #sentence test    
+    @pytest.mark.parametrize('b,result', [
+        ("bonjour grandpy, que sais tu sur la tour eiffel ?", 
+        "bonjour grandpy  que sais tu sur la tour eiffel  "),
+        ("salut grandpy ! est-ce que tu connais l'adresse d'openclassrooms ?",
+         "salut grandpy   est ce que tu connais l adresse d openclassrooms  "),
+        ("commènt%ù vas tu gràndpy^, ça va' bien o↓ pas ?",
+        "comment u vas tu grandpy   ca va  bien o pas  ")
+    ])
 
+    def test_rm_accents(self, b, result):
+        parser = ParseSentence(b)
+        assert parser.rm_accents() == result
